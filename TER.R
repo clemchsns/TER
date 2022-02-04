6ae74547a0d383291a34b2db2d5fc7b4796046bb
margauxbll <97876480+margauxbll@users.noreply.github.com>
2022-02-04 16:11:46 +0100

    maj

--- TER.R
+++ TER.R
@@ -3,10 +3,7 @@ data_base <- read.csv(file="inconnu pour l'instant.csv")
 
 summary(data_base)
 View(data_base)
-<<<<<<< HEAD
-=======
 
 clemence = 6
 clemence
 
->>>>>>> 99b6aa596b6ba11968da65454b540cf25e38607a
