ba285b9cffc71661503b49211d359c98128fad4f
orianeduclos <97462802+orianeduclos@users.noreply.github.com>
2022-02-28 10:10:34 +0100

    joueurs

--- TER.R
+++ TER.R
@@ -3,3 +3,8 @@ data_base <- read.csv("../Data/NBA_Season_Data.csv",sep=",",header =TRUE)
 View(data_base)
 
 
+data_players <-read.csv("../Data/players.csv",sep=",",header =TRUE)
+View(data_players)
+
+
+
