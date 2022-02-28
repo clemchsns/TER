721cf7ecde901afd753a6439c276af32c759d4e5
Marie <marieguibert.mg@gmail.com>
2022-02-28 10:41:03 +0100

    modif col

--- TER.R
+++ TER.R
@@ -2,7 +2,7 @@
 data_base <- read.csv("../Data/NBA_Season_Data.csv",sep=",",header =TRUE)
 View(data_base)
 
-colnames(data_base, "Année", "Équipe", "Joueur", "Age", "NbMatchs", "MinutesJouées", "NbPaniers", "PerfParMin", "EfficacitéTir", "Tentative3pts", "TentativesLancersFrancs", "PrctRebondOffensif", "PrctRebondDefensif", "NbTotalRebonds", "ContrôleBallon", "BallonsVolés", "BlocksParJeu", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "NbTirs", "JoueurID", "Supp", "Supp", "Supp", "EfficacitéTirÉquipe", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp")
+colnames(data_base) <- c("Année", "Équipe", "Joueur", "Age", "NbMatchs", "MinutesJouées", "NbPaniers", "PerfParMin", "EfficacitéTir", "Tentative3pts", "TentativesLancersFrancs", "PrctRebondOffensif", "PrctRebondDefensif", "NbTotalRebonds", "ContrôleBallon", "BallonsVolés", "BlocksParJeu", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "NbTirs", "JoueurID", "Supp", "Supp", "Supp", "EfficacitéTirÉquipe", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp")
 
 data_players <-read.csv("../Data/players.csv",sep=",",header =TRUE)
 View(data_players)
