762126952613fbf878d43fad2807d78c8c21d903
Marie <marieguibert.mg@gmail.com>
2022-02-28 10:34:27 +0100

    noms colonnes

--- TER.R
+++ TER.R
@@ -2,9 +2,9 @@
 data_base <- read.csv("../Data/NBA_Season_Data.csv",sep=",",header =TRUE)
 View(data_base)
 
+colnames(data_base, "Année", "Équipe", "Joueur", " ge", "NbMatchs", "MinutesJouées", "NbPaniers", "PerfParMin", "EfficacitéTir", "Tentative3pts", "TentativesLancersFrancs", "PrctRebondOffensif", "PrctRebondDefensif", "NbTotalRebonds", "ContrôleBallon", "BallonsVolés", "BlocksParJeu", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "NbTirs", "JoueurID", "Supp", "Supp", "Supp", "EfficacitéTirÉquipe", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp")
 
 data_players <-read.csv("../Data/players.csv",sep=",",header =TRUE)
 View(data_players)
-
 
 
