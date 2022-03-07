1693e64e7ff886758c776b44a34bf17f281e64e2
margauxbll <97876480+margauxbll@users.noreply.github.com>
2022-03-07 10:35:08 +0100

    ajout de la moyenne d'âge

--- TER.R
+++ TER.R
@@ -4,6 +4,8 @@ View(data_base)
 
 colnames(data_base) <- c("Année", "Équipe", "Joueur", "Age", "NbMatchs", "MinutesJouées", "NbPaniers", "PerfParMin", "EfficacitéTir", "Tentative3pts", "TentativesLancersFrancs", "PrctRebondOffensif", "PrctRebondDefensif", "NbTotalRebonds", "ContrôleBallon", "BallonsVolés", "BlocksParJeu", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "NbTirs", "JoueurID", "Supp", "Supp", "Supp", "EfficacitéTirÉquipe", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp", "Supp")
 data_base <- data_base[,c(1:17, 32:33, 37)]
+#moyenne d'âge pour chaque équipe
+tapply(data_base$Age, data_base$Équipe, mean)
 
 data_players <-read.csv("../Data/players.csv",sep=",",header =TRUE)
 View(data_players)
