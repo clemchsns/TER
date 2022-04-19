d4cf21ec9dbd68ca01f27d948762b661652df744
Marie <marieguibert.mg@gmail.com>
2022-04-19 16:02:19 +0200

    modification

--- TER_app/app.R
+++ TER_app/app.R
@@ -11,16 +11,13 @@ colnames(data_equipe) <- c("Annee", "Equipe", "Nom", "Age", "NbMatchs", "Minutes
 data_equipe <- data_equipe[,c(1:17, 31:32, 37)]
 data_equipe$Equipe = factor(data_equipe$Equipe)
 levels(data_equipe$Equipe)=c("Atlanta Hawks", "Boston Celtics","Brooklyn Nets","Buffalo Braves", "Charlotte Hornets", "Chicago Hustle", "Chicago Bulls","Chicago Bruins", "Cleveland Cavaliers","Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors","Houston Rockets","Indiana Pacers","Kings of Sacramento","Los Angeles Clippers","Los Angeles Lakers","Memphis Grizzlies","Miami Heat", "Milwaukee Bucks","Minnesota Timberwolves","Brooklyn Nets","New Orleans Hurricanes","New Orleans Jazz Roster ans Stats","New Orleans/Oklahoma City","New Orleans Pelicans","New York Knicks","Oklahoma City Thunder","Orlando Magic","Philadelphia 76ers", "Phoenix Suns","Portland Trail Blazers","Sacramento Kings","San Antonio Spurs", "San Diego Clippers","Seattle SuperSonics", "Toronto Raptors","Utah Jazz","Vancouver Grizzlies", "Washington Wizards","Washington Bullets") 
-#View(data_equipe)
-nrow(data_equipe)
 
 data_players <- read.csv("../Data/players.csv", header=TRUE, stringsAsFactors=TRUE)
 summary(data_players)
 colnames(data_players) = c("supp","DateNaiss","LieuNaiss","Supp","Supp","supp","Supp","Supp","Supp","Supp","Supp","Supp","Supp","Universite","Supp","Supp","Equipe","Ann?ePro","Taille","Lycee","Nom","position","MainTire","Poids")
 data_players = data_players[,c(2:3,14,17:24)]
-#View(data_players)
-nrow(data_players)
 
+#création dataframe pour pouvoir sélectionner les joueurs
 joueurs_names = data_base$Nom
 
 # Trouver les correspondances entre data et data_players 
@@ -34,11 +31,8 @@ ui <- dashboardPage(
         sidebarMenu(
             menuItem("Accueil", tabName = "Accueil", icon = icon("basketball-ball")),
             menuItem("Clubs", tabName = "Clubs", icon = icon("running")),
-            menuItem("Joueurs", tabName = "Joueurs", icon = icon("arrow-right")),
-            menuItem("Base de données des clubs", tabName = "DonClubs", icon = icon("arrow-right")),
-            menuItem("Base de données des joueurs", tabName = "DonJoueurs", icon = icon("arrow-right"))
-        )
-    ),
+            menuItem("Joueurs", tabName = "Joueurs", icon = icon("arrow-right"))
+    )),
     dashboardBody(
         tabItems(
             tabItem("Accueil",
@@ -70,10 +64,6 @@ ui <- dashboardPage(
                                                           )), verbatimTextOutput("j"), dataTableOutput("cara")),
                                   tabPanel('Position du joueur')
                               ))),
-            tabItem("DonClubs",
-                    fluidPage(h1("Base de données des Clubs"),verbatimTextOutput('view1'))),
-            tabItem("DonJoueurs",
-                    fluidPage(h1("Base de données des joueurs"),verbatimTextOutput('view2')))
         )
     )
 )
@@ -103,31 +93,6 @@ server <- function(input, output) {
     
     output$cara <- renderDataTable(data_base[which(data_base$Nom==input$joueurs_id),][,c(1:2,4:18,20)])
     
-    #output$E <- renderText({
-    #    paste("Equipe dans lesquelles le joueur a été")
-    #    paste(unique(data_base$Equipe[data_base$Nom==input$joueurs_id]),sep="-")
-    #})
-    
-    #output$Tirs <- renderPrint({
-    #    data_base$EfficaciteTir[data_base$Nom==input$joueurs_id & data_base$Annee==2015]
-    #})
-    #output$NbTirs <- renderPrint({
-    #    data_base$NbTirs[data_base$Nom==input$joueurs_id & data_base$Annee==2015]
-    #})
-    
-    #
-    #data_base$NbMatchs[data_base$Joueur==input$varcj & data_base$Annee==2015,]
-    #data_base$MinutesJouees[data_base$Joueur==input$varcj & data_base$Annee==2015,]
-    
-    #Afficher la base de données des clubs
-    #output$view1 <- renderPrint({
-    #    data_base
-    #})
-    #Afficher la base de données des joueurs
-    #output$view2 <- renderPrint({
-    #data_players
-    #})
-    
     output$textcj <- renderText({
         paste("Voici les joueurs ayant été dans le club", input$varcj,"entre 1978 et 2015")
     })
@@ -142,7 +107,6 @@ server <- function(input, output) {
         ggplot(data_base1)+aes(x=Annee,y=EfficaciteTirEquipe)+
             geom_point()+geom_smooth()+theme_bw()
     })
-    
     
     #Camembert
     output$varpie <- renderPlot({
